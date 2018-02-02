5aada84c550e85751fe5e2c75ea2e7376298f817
Ben Gardiner <ben.l.gardiner@gmail.com>
2018-02-02 11:07:30 -0500

    add Jonathan's trick to fixing up the dumb drefs made by the script if the target line is renamed

--- atxmega128a4u/scripts/avr_dumb_seq_load_xrefs.py
+++ atxmega128a4u/scripts/avr_dumb_seq_load_xrefs.py
@@ -72,6 +72,13 @@ try:
             prev = line
             continue
 
+        def safe_name(address):
+            name = sark.Line(address).name
+            if name[0:4] == 'unk_':
+                 return "0x%x" % address
+            else:
+                return  "%s" % name
+
         logger.debug("testing @ 0x%x %s == %s" % (line.ea, prev_insn.operands[0].reg, rpairs.get(curr_insn.operands[0].reg)))
         if prev_insn.operands[0].reg == rpairs.get(curr_insn.operands[0].reg):
                 word = int(curr_insn.operands[1].text, 0) * 256 + int(prev_insn.operands[1].text, 0)
@@ -80,12 +87,28 @@ try:
                 if address <= ram_segment.endEA:
                     result = add_dref(line.ea, address, dr_T)
                     logger.debug("add dref from 0x%x to 0x%x: %s" % (line.ea, address, str(result)))
-                    name = sark.Line(address).name
-                    if name[0:4] == 'unk_':
-                        line.comments.repeat = "0x%x" % address
-                    else:
-                        line.comments.repeat = "%s" % name
+                    line.comments.repeat = safe_name(address)
+
         prev = line
+
+        def dref_range_fixer(startEA, endEA):
+            for line in sark.lines(startEA, endEA):
+                for xref in line.xrefs_to:
+                    if xref.iscode or xref.frm == idc.BADADDR or str(xref.type) != 'Data_Text': # only try to fix data references from code in ROM of the Data_Text type (as created by the dumb seq xref routine above)
+                        continue
+                    logger.debug("fixing xref (type:%s) to %s from 0x%x" % (xref.type, safe_name(line.ea), xref.frm))
+                    sark.Line(xref.frm).comments.repeat = safe_name(line.ea)
+            return
+
+        def dref_fixer():
+            dref_range_fixer(SelStart(), SelEnd())
+            return
+
+        def dref_all_fixer():
+            dref_range_fixer(ram_segment.startEA, ram_segment.endEA)
+            return
+
+    print("data xrefs (drefs) added for all sequential loads of the bytes 16-bit addresses into RAM.\nIf any lines are renamed in the RAM segment, then the drefs can be fixed by running dref_fixer() on a selection of ram addresses or dref_all_fixer() to perform this operation over all of the RAM segment.")
 
 except:
     exc_type, exc_value, exc_traceback = sys.exc_info()
